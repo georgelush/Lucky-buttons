0a419c91055ed94f83f98fd2e85739838fff5b74
georgelush <100272529+georgelush@users.noreply.github.com>
2022-03-03 17:01:35 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let luckybtn = Math.floor((Math.random() * 3) + 1);
-
-function luckyButton(idBtn) {
-  if (luckybtn == idBtn) {
-    document.getElementById("result").innerHTML ="Congratulations you won!";
-  } else {
-    document.getElementById("result").innerHTML ="I'm sorry :( to try again";
-  }
-}
